0a5c546ee7a70490be9ba3d25d75410eb70820f8
woc917 <44800016+woc917@users.noreply.github.com>
2019-09-01 21:23:34 +1000

    Add files via upload

--- assets/js/newTagHide.js
+++ assets/js/newTagHide.js
@@ -4,6 +4,6 @@ $(document).ready(function(){
 
   $('.new290819').hide();
   $('.new300819').hide();
-  $('.new310819')..text('1 day ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new310819').text('1 day ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
 });
