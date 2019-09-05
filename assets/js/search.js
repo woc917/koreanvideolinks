f7f8ddbe133a96bec6ae486a2543dd77f29413cc
woc917 <44800016+woc917@users.noreply.github.com>
2019-09-05 12:16:21 +1000

    Add files via upload

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
       content.parent().hide();
       var temp = $("h5:contains('" + k + "')");
       temp.parents('div').show();
+      $('.mobAd').hide();
+
   })
 
     // 원인 발견! hide() 시킨 그 태그를 정확히 선택해서 다시 보이기 해야 함.
