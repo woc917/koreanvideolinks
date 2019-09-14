5e027abff60a3f1964fedaefdbd979af5b505049
woc917 <44800016+woc917@users.noreply.github.com>
2019-09-14 19:26:14 +1000

    Add files via upload

--- assets/js/newTagHide.js
+++ assets/js/newTagHide.js
@@ -4,18 +4,19 @@ $(document).ready(function(){
 
   $('.new290819').text('2 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new300819').text('2 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new310819').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new310819').text('2 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new010919').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new020919').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new030919').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new040919').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new050919').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new060919').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new070919').text('6 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new080919').text('5 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new090919').text('4 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new100919').text('3 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new110919').text('2 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new120919').text('1 day ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new070919').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new080919').text('6 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new090919').text('5 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new100919').text('4 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new110919').text('3 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new120919').text('2 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new130919').text('1 day ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
 });
