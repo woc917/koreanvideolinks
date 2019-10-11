bd2082b1722f7a900291f6569a2df14e5f63e00d
woc917 <44800016+woc917@users.noreply.github.com>
2019-10-11 23:43:28 +1100

    Add files via upload

--- assets/js/newTagHide.js
+++ assets/js/newTagHide.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
   $('.new290819').text('6 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new300819').text('6 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  
+
   $('.new310819').text('5 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new010919').text('5 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new020919').text('5 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
@@ -50,6 +50,6 @@ $(document).ready(function(){
   $('.new071019').text('4 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new081019').text('3 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new091019').text('2 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new091019').text('1 day ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new101019').text('1 day ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
 });
