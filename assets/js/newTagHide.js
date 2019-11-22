bcb90ce642b1cdf4718bc6dcb67ed19d93e5d50b
woc917 <44800016+woc917@users.noreply.github.com>
2019-11-22 13:21:44 +0900

    Add files via upload

--- assets/js/newTagHide.js
+++ assets/js/newTagHide.js
@@ -1,98 +1,100 @@
 $(document).ready(function(){
 
-  $('.new191119').text('1 day ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new181119').text('2 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new171119').text('3 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new161119').text('4 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new151119').text('5 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new141119').text('6 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new201119').text('1 day ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new191119').text('2 day ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new181119').text('3 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new171119').text('4 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new161119').text('5 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+  $('.new151119').text('6 days ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
+  $('.new141119').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new131119').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new121119').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new111119').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new101119').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new091119').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new081119').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new071119').text('1 week ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
+  $('.new071119').text('2 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new061119').text('2 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new051119').text('2 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new041119').text('2 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new031119').text('2 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new021119').text('2 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new011119').text('2 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new311019').text('2 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
+  $('.new311019').text('3 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new301019').text('3 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new291019').text('3 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new281019').text('3 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new271019').text('3 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new261019').text('3 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new251019').text('3 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new241019').text('3 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
+  $('.new241019').text('4 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new231019').text('4 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new221019').text('4 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new211019').text('4 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new201019').text('4 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new191019').text('4 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new181019').text('4 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new171019').text('4 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
+  $('.new171019').text('5 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new161019').text('5 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new151019').text('5 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new141019').text('5 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new131019').text('5 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new121019').text('5 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new111019').text('5 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new101019').text('5 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
+  $('.new101019').text('6 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new091019').text('6 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new081019').text('6 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new071019').text('6 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new061019').text('6 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new051019').text('6 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new041019').text('6 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new031019').text('6 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
+  $('.new031019').text('7 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new021019').text('7 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new011019').text('7 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new300919').text('7 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new290919').text('7 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new280919').text('7 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new270919').text('7 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new260919').text('7 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
+  $('.new260919').text('8 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new250919').text('8 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new240919').text('8 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new230919').text('8 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new220919').text('8 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new210919').text('8 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new200919').text('8 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new190919').text('8 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
+  $('.new190919').text('9 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new180919').text('9 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new170919').text('9 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new160919').text('9 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new150919').text('9 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new140919').text('9 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new130919').text('9 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new120919').text('9 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
+  $('.new120919').text('10 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new110919').text('10 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new100919').text('10 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new090919').text('10 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new080919').text('10 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new070919').text('10 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new060919').text('10 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new050919').text('10 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
+  $('.new050919').text('11 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new040919').text('11 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new030919').text('11 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new020919').text('11 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new010919').text('11 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new310819').text('11 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
   $('.new290819').text('11 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
-  $('.new300819').text('11 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
+
+  $('.new300819').text('12 weeks ago').css('color', '#4F4F4F').css('background', 'none').css('opacity', '0.8').css('font-weight', 'bold');
 
 });
